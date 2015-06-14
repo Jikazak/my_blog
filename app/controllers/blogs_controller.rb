87ec70607d7f28b1ea6982f7d7db2b98d378f9f6
Junya Okazaki <junya.okazaki@gmail.com>
2015-06-14 11:17:57 +0900

    create controller added

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -3,7 +3,28 @@ class BlogsController < ApplicationController
     @post = Post.new
   end
 
-  def create
+  def show
+    @post = Post.find(params[:id])
+  end
 
+  def create
+    # formからなデータを受け取る
+    @post = Post.new(post_params)
+    # これを保存する
+    @post.save #saveをしたら、id, created_at, updated_atが加えられる
+    # show.html.erbに飛ばす
+    redirect_to "/blogs/#{@post.id}"
+  end
+
+
+
+  private
+  def post_params
+        # params.require(:key).permit(:filter)
+    params.require(:post).permit(  #フォームに入力されたデータを取得して、フィルタリング
+      :title,
+      :category,
+      :body
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
   get 'blogs/new' => 'blogs#new'
+  get 'blogs/:id' => 'blogs#show'
   post 'blogs' => 'blogs#create'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
